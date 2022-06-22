4a51174bd513b1c4031d79f0f5105374dd72aa63
Ralph <raftsdev@gmail.com>
2022-06-21 20:04:41 -0400

    Chore: Typo

--- core/src/main/java/academy/learnprogramming/Main.java
+++ core/src/main/java/academy/learnprogramming/Main.java
@@ -8,5 +8,6 @@ public class Main {
     private static final Logger log = LoggerFactory.getLogger(Main.class);
     public static void main(String[] args) {
         log.info("Guess the number game");
+        
     }
 }
